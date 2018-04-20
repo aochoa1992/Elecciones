93027ce429f5cf28c75caee29210e5f41e5429c7
Yhony Ochoa <yhonyochoa@mattelsa.net>
2018-04-20 14:04:12 -0500

    creaciones de migradores

---
+++ src/Elecciones.EntityFrameworkCore/Migrations/20180420165220_InsertsDefault.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Elecciones.Migrations
+{
+    public partial class InsertsDefault : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"INSERT INTO [dbo].[AbpRoles]([ConcurrencyStamp],[CreationTime],[CreatorUserId],[DisplayName],[IsDefault],[IsDeleted],[IsStatic],[Name],[NormalizedName],[Description]) VALUES
+            (Convert(nvarchar(max),NEWID()),GETDATE(),NULL,'Candidato',0,0,1,'Candidato','Candidato','Candidato'),
+		    (Convert(nvarchar(max),NEWID()),GETDATE(),NULL,'Elector',0,0,1,'Elector','Elector','Elector')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
